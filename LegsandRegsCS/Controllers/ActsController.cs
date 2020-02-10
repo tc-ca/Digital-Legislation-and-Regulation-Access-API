2c3fe707cf52f7278bc9f0f300f3af38dd6aba5a
Lawrence Maroun <lawrence.maroun@034gc.onmicrosoft.com>
2020-02-10 09:51:01 -0500

    Add project files.

---
+++ LegsandRegsCS/Controllers/ActsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LegsandRegsCS.Controllers
+{
+    [Route("getActs")]
+    [ApiController]
+    public class ActsController : ControllerBase
+    {
+        // GET: api/Acts
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Acts/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value" + id as String;
+        }
+
+        // POST: api/Acts
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Acts/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
